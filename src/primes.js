2887ba2cf130c8fb7c8a7fe5aded65f522a5f65c
Drew Hinderhofer <drew.hinderhofer@gmail.com>
2023-04-03 20:46:04 -0400

    initial commit and js primes

---
+++ src/primes.js
@@ -0,0 +1,21 @@
+function generatePrimes(limit = 1000) {
+    console.log(`generating primes through ${limit}`);
+    console.time("generating primes");
+    const primes = [1, 2];
+    for (let i = 3; i < limit; i += 2) {
+        let prime = true;
+        for (let j = 1; j < primes.length; j++) {
+            if (i % primes[j] === 0) {
+                prime = false;
+                break;
+            }
+        }
+        if (prime) {
+            primes.push(i);
+        }
+    }
+    console.timeEnd("generating primes");
+    console.log(`generated ${primes.length} primes`);
+}
+
+generatePrimes();
